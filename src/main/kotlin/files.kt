7f38f918330837bae1661b0de97f735bcf6cd6db
Zhenya <standup.minsk@gmail.com>
2026-02-15 23:32:57 +0300

    KTB-04 removing unnecessary actions

--- src/main/kotlin/files.kt
+++ src/main/kotlin/files.kt
@@ -8,9 +8,6 @@ data class Word(val text: String, val translate: String, var correctAnswersCount
 fun main() {
     val wordsFile = File("words.txt")
     wordsFile.createNewFile()
-    wordsFile.writeText("hello|привет|\n")
-    wordsFile.appendText("dog|собака|\n")
-    wordsFile.appendText("cat|кошка|\n")
 
     val dictionary = mutableListOf<Word>()
 
