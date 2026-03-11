0a76a2586ec69853075334dbefb1e3010ae8ebc5
Zhenya <standup.minsk@gmail.com>
2026-03-11 15:51:29 +0300

    KTB-27

--- src/main/kotlin/Telegram.kt
+++ src/main/kotlin/Telegram.kt
@@ -46,6 +46,24 @@ data class Document(
 )
 
 @Serializable
+data class SendPhotoResponse(
+    val ok: Boolean,
+    val result: Message? = null
+)
+
+@Serializable
+data class PhotoSize(
+    @SerialName("file_id")
+    val fileId: String,
+    @SerialName("file_unique_id")
+    val fileUniqueId: String,
+    @SerialName("file_size")
+    val fileSize: Long,
+    val width: Int,
+    val height: Int
+)
+
+@Serializable
 data class Message(
     @SerialName("text")
     val text: String? = null,
@@ -53,6 +71,9 @@ data class Message(
     val chat: Chat,
     @SerialName("document")
     val document: Document? = null,
+    @SerialName("message_id")
+    val messageId: Long,
+    val photo: List<PhotoSize>? = null,
 )
 
 @Serializable
@@ -209,7 +230,7 @@ fun handleUpdates(
             val newWords = mutableListOf<Word>()
             for (word in File(downloadedFile).readLines()) {
                 val parts = word.split("|")
-                newWords.add(Word(parts[0], parts[1], parts[2].toIntOrNull() ?: 0))
+                newWords.add(Word(parts[0], parts[1], parts[2].toIntOrNull() ?: 0, parts[3], parts[4]))
             }
 
             newWords.forEach { word ->
@@ -219,6 +240,19 @@ fun handleUpdates(
             }
 
             trainer.saveDictionary()
+        }
+    }
+
+    trainer.dictionary.forEach { word ->
+        if (word.imagePath != null && word.imageFileId == null) {
+            val file = File(word.imagePath)
+            if (file.exists()) {
+                val fileId = telegramBotService.sendPhoto(file, chatIdMatchResult, json, true, word)
+                if (fileId != null) {
+                    word.imageFileId = fileId
+                    trainer.saveDictionary()
+                }
+            }
         }
     }
 }
